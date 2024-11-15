b82c719caebc009b4c89f655560c0d2cee6cb4a3
Georgia Martinez <georgiacmartinez@gmail.com>
2024-11-15 14:34:40 -0500

    Created get_staff_coords function

--- staff.py
+++ staff.py
@@ -1,7 +1,31 @@
 import cv2
 import numpy as np
 
-def detect_horizontal_lines(image, min_line_length=100, max_line_gap=20):
+def get_staff_coords(image_url, debug=False):
+    """
+    Returns the y coords for the 5 lines in the staff
+    """
+
+    image = cv2.imread(image_url)
+
+    open_cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+    line_image, horizontal_lines = get_horizontal_lines(open_cv_image)
+
+    if debug:
+        cv2.imshow(f"Horizontal Lines", line_image)
+        cv2.waitKey(0)
+
+    y_coords = set()
+
+    for line in horizontal_lines:
+        x1, y1, x2, y2 = line
+
+        y_coords.add(y1)
+
+    return y_coords
+
+def get_horizontal_lines(image, min_line_length=100, max_line_gap=20):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
@@ -26,22 +50,3 @@ def detect_horizontal_lines(image, min_line_length=100, max_line_gap=20):
                 cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
     
     return line_image, horizontal_lines
-
-
-image = cv2.imread("test.png")
-
-open_cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-
-line_detected_image, horizontal_lines = detect_horizontal_lines(open_cv_image)
-
-cv2.imshow(f"Horizontal Lines", line_detected_image)
-cv2.waitKey(0)
-
-y_coords = set()
-
-for line in horizontal_lines:
-    x1, y1, x2, y2 = line
-
-    y_coords.add(y1)
-
-print(len(y_coords))
