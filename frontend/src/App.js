9cb74ce20bcc90476669518d0fa6bccef4e4721c
Isabella Wloch <isabellawloch@Isabellas-MacBook-Pro.local>
2024-11-15 15:25:59 -0500

    added buttons for basic notes using tonejs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import * as Tone from "tone";
 
 const App = () => {
   const [message, setMessage] = useState("");
@@ -21,11 +22,48 @@ const App = () => {
     }
   };
 
+  const synth = new Tone.Synth().toDestination();
+
+  const playNoteA = () => {
+    synth.triggerAttackRelease("A3", "8n");
+  }
+
+  const playNoteB = () => {
+    synth.triggerAttackRelease("B3", "8n");
+  }
+
+  const playNoteC = () => {
+    synth.triggerAttackRelease("C4", "8n");
+  }
+
+  const playNoteD = () => {
+    synth.triggerAttackRelease("D4", "8n");
+  }
+
+  const playNoteE = () => {
+    synth.triggerAttackRelease("E4", "8n");
+  }
+
+  const playNoteF = () => {
+    synth.triggerAttackRelease("F4", "8n");
+  }
+
+  const playNoteG = () => {
+    synth.triggerAttackRelease("G4", "8n");
+  }
+
   return (
       <div>
         <h1>React and Flask Integration</h1>
         <input type="file" onChange={handleFileUpload} />
         <p>{message}</p>
+        <button onClick={playNoteA}>A</button>
+        <button onClick={playNoteB}>B</button>
+        <button onClick={playNoteC}>C</button>
+        <button onClick={playNoteD}>D</button>
+        <button onClick={playNoteE}>E</button>
+        <button onClick={playNoteF}>F</button>
+        <button onClick={playNoteG}>G</button>
       </div>
   );
 };
