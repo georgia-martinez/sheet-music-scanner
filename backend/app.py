96553b0a54f2d7966730e91fa02bffe29fe79288
justin-metti <155192968+justin-metti@users.noreply.github.com>
2025-01-21 16:00:19 -0500

    Added some database config stuff

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,26 @@ from music_scanner import scan_music
 app = Flask(__name__)
 CORS(app)
 
+# Database configuration
+## TODO Figure out shit shit
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Atomic1@localhost/music_processor'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 # Define the directory for saving uploaded files
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Create directory if it doesn't exist
+
+
+class Songs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    song_name = db.Column(db.String(255), nullable=False)
+    artist = db.Column(db.String(255), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    processed_json = db.Column(db.JSON, nullable=False)
+    uploaded_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 @app.route('/process-image', methods=['POST'])
 def process_image():
